4b97718849a6d225420af859ea43a59217f3cdb4
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-13 11:50:13 +0100

    paging start

--- index.js
+++ index.js
@@ -11,18 +11,36 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
-async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+async function fetchCharacters(page) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
   const characters = await response.json();
-  console.log(characters.results);
+  console.log(characters);
+  cardContainer.innerHTML = "";
+  maxPage = characters.info.pages;
+  console.log("maxPage", maxPage);
+  pagination.textContent = `${page} / ${maxPage}`;
   characters.results.forEach((character) => {
     const cardList = createCharacterCard(character);
     cardContainer.append(cardList);
   });
 }
+fetchCharacters(page);
+nextButton.addEventListener("click", () => {
+  page++;
+  console.log("page", page);
+  fetchCharacters(page);
+});
+prevButton.addEventListener("click", () => {
+  /*  console.log("prev",characters.info.prev);*/
+  page--;
 
-fetchCharacters();
+  console.log("page", page);
+  fetchCharacters(page);
+  /* characters.info.prev ===null?prevButton.disabled: */
+});
