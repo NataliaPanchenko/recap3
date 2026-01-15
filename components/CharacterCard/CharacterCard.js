f0d712f53655bb3f32549db64f11e104843580f3
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-15 15:56:33 +0100

    refactoring

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,24 +1,24 @@
-export function createCharacterCard(cardItem) {
+export function createCharacterCard(characterData) {
   const card = document.createElement("li");
   card.classList.add("card");
   card.innerHTML = `
             <div class="card__image-container">
                 <img
                 class="card__image"
-                src=${cardItem.image}
-                alt="Rick Sanchez"
+                src=${characterData.image}
+                alt="${characterData.name}"
                 />
                 <div class="card__image-gradient"></div>
             </div>
             <div class="card__content">
-                <h2 class="card__title">${cardItem.name}</h2>
+                <h2 class="card__title">${characterData.name}</h2>
                 <dl class="card__info">
                 <dt class="card__info-title">Status</dt>
-                <dd class="card__info-description">${cardItem.status}</dd>
+                <dd class="card__info-description">${characterData.status}</dd>
                 <dt class="card__info-title">Type</dt>
-                <dd class="card__info-description">${cardItem.type}</dd>
+                <dd class="card__info-description">${characterData.type}</dd>
                 <dt class="card__info-title">Occurrences</dt>
-                <dd class="card__info-description">${cardItem.episode.length}</dd>
+                <dd class="card__info-description">${characterData.episode.length}</dd>
                 </dl>
             </div>
             `;
